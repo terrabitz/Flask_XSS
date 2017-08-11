3699af8f5e3150efa0f34ef62a6dd40d724fbe0f
terrabitz <trevor.taubitz@gmail.com>
2017-08-11 18:37:00 -0400

    Added bootswatch styling and fixed XSS protection issue

--- views/__init__.py
+++ views/__init__.py
@@ -20,7 +20,7 @@ def index():
     return render_template('unsafe_params.html', test=test), 200
 
 
-@unsafe_params_no_protection.after_app_request
+@unsafe_params_no_protection.after_request
 def remove_xss_protection(response):
     response.headers['X-XSS-Protection'] = 0
     return response
