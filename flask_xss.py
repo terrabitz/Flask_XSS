39bcfd928fe2ce66d4bb19d0d6589aa2c0bfefd9
terrabitz <trevor.taubitz@gmail.com>
2017-08-12 12:26:58 -0400

    Refactored components

--- flask_xss.py
+++ flask_xss.py
@@ -1,22 +1,37 @@
 from flask import Flask, render_template, request, send_from_directory, url_for
+from copy import deepcopy
 
 from utils import snake_to_title
 from views import (
     unsafe_params,
-    unsafe_params_no_protection,
+    unsafe_cookies,
+    remove_xss_protection
 )
-
 
 NO_PROTECTION_PREFIX = '/no_protec'
 
 app = Flask(__name__)
 
-app.register_blueprint(unsafe_params_no_protection, url_prefix=NO_PROTECTION_PREFIX + '/unsafe_params')
-app.register_blueprint(unsafe_params, url_prefix='/unsafe_params')
+blueprint_paths = {
+    unsafe_params: "/unsafe_params",
+    unsafe_cookies: "/unsafe_cookies"
+}
+
+# Register all browser-protected blueprints
+for blueprint, path in blueprint_paths.items():
+    app.register_blueprint(blueprint, url_prefix=path)
+
+# Create unprotected versions of each blueprint
+for blueprint, path in blueprint_paths.items():
+    blueprint_copy = deepcopy(blueprint)
+    blueprint_copy.name += "_no_protection"
+    blueprint_copy.after_request(remove_xss_protection)
+    app.register_blueprint(blueprint_copy, url_prefix=NO_PROTECTION_PREFIX + path)
 
 
 @app.route('/')
 def index():
+    """Create an index of all available sandboxes"""
     pages = {snake_to_title(bp): url_for(bp + '.index') for bp in app.blueprints.keys()}
     return render_template('index.html', pages=pages, enumerate=enumerate, len=len)
 
--- views/__init__.py
+++ views/__init__.py
@@ -1,14 +1,4 @@
-from flask import request, render_template, Blueprint
-
-unsafe_params_no_protection = Blueprint('unsafe_params_no_protection', __name__, )
-
-
-@unsafe_params_no_protection.route('/')
-def index():
-    """Renders a page"""
-    test = request.args.get('name', ' world!')
-    return render_template('unsafe_params.html', test=test), 200
-
+from flask import request, render_template, Blueprint, current_app
 
 unsafe_params = Blueprint('unsafe_params', __name__)
 
@@ -16,11 +6,39 @@ unsafe_params = Blueprint('unsafe_params', __name__)
 @unsafe_params.route('/')
 def index():
     """Renders a page with XSS flags enabled to use browser-based defenses"""
-    test = request.args.get('name', ' world!')
-    return render_template('unsafe_params.html', test=test), 200
+    title = 'Unsafe Parameters'
+    description = 'A GET parameter is unsafely handled. Try entering something in the text box, or setting ' \
+                  '"?name=something" in the header'
+
+    name = request.args.get('name', 'world!')
+    return render_template('unsafe_params.html', name=name, title=title, description=description)
 
 
-@unsafe_params_no_protection.after_request
+unsafe_cookies = Blueprint('unsafe_cookies', __name__)
+
+
+@unsafe_cookies.route('/', methods=["GET", "POST"])
+def index():
+    """Renders a page that processes cookies in an unsafe way"""
+    title = 'Unsafe Cookies'
+    description = 'A cookie called "name" is unsafely handled. Try setting it using a cookie editor, or by enter a ' \
+                  'new name in the text box below'
+
+    cookie_name = request.cookies.get('name', 'world')
+    new_name = request.form.get('name', None)
+    if new_name:
+        # Form was submitted, change  cookie
+        name = new_name
+    else:
+        # Use previous cookie
+        name = cookie_name
+
+    response = current_app.make_response(
+        render_template('unsafe_cookies.html', name=name, title=title, description=description))
+    response.set_cookie('name', name)
+    return response
+
+
 def remove_xss_protection(response):
     response.headers['X-XSS-Protection'] = 0
     return response
