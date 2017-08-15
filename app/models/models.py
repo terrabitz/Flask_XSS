877e2957fba8b355872d752b0a9a6a35217bda66
terrabitz <trevor.taubitz@gmail.com>
2017-08-15 11:02:53 -0400

    Do db test upgrade

--- app/models/models.py
+++ app/models/models.py
@@ -7,10 +7,12 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True)
     password = db.Column(db.String(200))
+    email = db.Column(db.String(50))
 
-    def __init__(self, username, password):
+    def __init__(self, username, password, email=""):
         self.username = username
         self.password = password
+        self.email = email
 
     @property
     def is_admin(self):
